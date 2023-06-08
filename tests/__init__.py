7bb1967e4d911aba5da306ac7533f2ba96755166
Tom Baker <tom@tombaker.org>
2023-06-08 17:31:36 +0200

    defdir.txt

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,5 @@
-"""Tells pytest to go up one directory:
-* to root of test directory
-* to look for pytest.ini"""
+"""
+Tells pytest to go up one directory:
+- to root of test directory
+- to look for pytest.ini
+"""
