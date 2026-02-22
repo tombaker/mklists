6de0b0b2f02c6002a582912ca36056c53212bdac
Tom Baker <tom@tombaker.org>
2026-02-22 12:49:31 +0100

    structure/__init__.py

--- src/mklists/structure/__init__.py
+++ src/mklists/structure/__init__.py
@@ -1,8 +1,10 @@
+"""Convenience exports for structural types, resolvers, and marker names."""
+
 from .contexts_datadir import DatadirContext, resolve_datadir_context
 from .contexts_run import RunContext, resolve_run_context
 from .markers import (
-    DATADIR_CONFIGFILE_NAME, 
-    DATADIR_RULEFILE_NAME, 
-    REPO_CONFIGFILE_NAME, 
+    DATADIR_CONFIGFILE_NAME,
+    DATADIR_RULEFILE_NAME,
+    REPO_CONFIGFILE_NAME,
     REPO_RULEFILE_NAME,
 )
