da416a94c41216368201cd0cb5f23bd00f6abd6b
Tom Baker <tom@tombaker.org>
2026-02-22 07:04:36 +0100

    Markers for directory type

--- src/mklists/structure.py
+++ src/mklists/structure.py
@@ -1,13 +1,14 @@
-"""Marker filenames for Repo Roots and Datadirs
+"""Markers for directory types
 
-A directory may be defined structurally as a Repo Root or a Datadir: 
+Certain marker files, when present, define a directory structurally as a Repo Root 
+or Datadir: 
 -  A Repo Root is marked by the presence of `mklists.yaml` and/or `mklists.rules`.
 -  A Datadir is marked by the presence of `.rules`.
 -  A directory cannot be both a Repo Root and a Datadir.
 
-A Datadir may be marked as "self-contained" by the presence of `.mklistsrc`. It is
-processed without reference to any rules or config settings defined in a Repo Root, 
-even if one exists.
+A Datadir is marked as self-contained if `.mklistsrc` is present. A self-contained
+Datadir is processed without reference to repo-level rules or configuration, even if
+a Repo Root exists.
 """
 
 DATADIR_CONFIGFILE_NAME = ".mklistsrc"
